e6368a6b5d0a48f0d8e4794c86ce3222385f879a
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2020-10-10 02:54:05 -0300

    implementando acoes na ticket confirm

--- src/app/pages/ticket-confirm/ticket-confirm.page.ts
+++ src/app/pages/ticket-confirm/ticket-confirm.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-ticket-confirm',
@@ -7,9 +8,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicketConfirmPage implements OnInit {
 
-  constructor() { }
+
+  baggages: number = 0;
+  baggagePrice: number = 150;
+  baggagePriceTotal: number = 0;
+
+  hasFood: boolean = false;
+  food: number = 150;
+  hotelNights: number = 0;
+  hotelNightPrice: number = 200;
+  hotelPriceTotal: number = 0;
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  incrementBaggage() {
+    this.baggages++;
+    this.calculateBaggagePrice();
+  }
+
+  decrementBaggage() {
+    if (this.baggages > 0) {
+      this.baggages--;
+      this.calculateBaggagePrice();
+    }
+  }
+
+  calculateBaggagePrice() {
+    this.baggagePriceTotal = this.baggagePrice * this.baggages;
+  }
+
+  incrementHotelNight() {
+    this.hotelNights++;
+    this.calculateHotelPrice();
+  }
+
+  decrementHotelNight() {
+    if (this.hotelNights > 0) {
+      this.hotelNights--;
+      this.calculateHotelPrice();
+    }
+  }
+
+  calculateHotelPrice() {
+    this.hotelPriceTotal = this.hotelNightPrice * this.hotelNights;
+  }
+
+  goToPaymentConfirm() {
+    this.router.navigateByUrl('payment-confirm');
+  }
+
+  changeFood() {
+    if (this.hasFood)
+      this.hotelPriceTotal -= this.food;
+    else
+      this.hotelPriceTotal += this.food;
+  }
 }
