089e22e66e3af724019442df978808211f1ffae8
DevLucasOliveira <lucasoliveira.si@outlook.com>
2020-10-09 23:04:25 -0300

    Salvando busca da passagem do usuário

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,3 +1,5 @@
+import { TicketService } from './../../services/ticket.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: TicketService
+  ) { }
 
   ngOnInit() {
+    this.buildForm();
+  }
+
+  buildForm(){
+    this.form = this.formBuilder.group({
+      origin: [''],
+      destiny: [''],
+      travelDate: [''],
+      returnDate: [''],
+      totalPeople: [''],
+      onlyTravel: [''],
+    });
+  }
+
+  search() {
+    var model = {
+      origin: this.form.controls.origin.value,
+      destiny: this.form.controls.destiny.value,
+      travelDate: this.form.controls.travelDate.value,
+      returnDate: this.form.controls.returnDate.value,
+      totalPeople: this.form.controls.totalPeople.value,
+      onlyTravel: false,
+    }
+
+    this.save(model);
+  }
+
+
+  save(model){
+    console.log(model);
+    this.service.addTicket(model).subscribe(
+      (result) => {
+        console.log(result);
+      },
+      (err) => {
+        console.log(err);
+      });
   }
 
 }
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
+import { HttpService } from './services/http.service';
 
 @NgModule({
     imports: [
@@ -9,5 +10,6 @@ import { RouterModule } from '@angular/router';
         IonicModule,
         RouterModule
     ],
+    providers: [HttpService]
 })
 export class SharedModule { }
