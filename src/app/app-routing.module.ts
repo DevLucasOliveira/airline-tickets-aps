6ae912ecf10e8000569e4c717da427669e0ec388
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2020-11-03 20:16:36 -0300

    criando menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,40 +5,37 @@ import { AppComponent } from './app.component';
 const routes: Routes = [
   {
     path: 'home',
-    component: AppComponent,
     loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
   },
   {
     path: 'ticket-list',
-    component: AppComponent,
     loadChildren: () => import('./pages/ticket-list/ticket-list.module').then(m => m.TicketListPageModule)
   },
   {
     path: 'ticket-confirm',
-    component: AppComponent,
     loadChildren: () => import('./pages/ticket-confirm/ticket-confirm.module').then(m => m.TicketConfirmPageModule)
   },
   {
     path: 'payment',
-    component: AppComponent,
     loadChildren: () => import('./pages/payment/payment.module').then(m => m.PaymentPageModule)
   },
   {
     path: 'payment-confirm',
-    component: AppComponent,
     loadChildren: () => import('./pages/payment-confirm/payment-confirm.module').then(m => m.PaymentConfirmPageModule)
   },
   {
     path: 'ticket-success',
-    component: AppComponent,
     loadChildren: () => import('./pages/ticket-success/ticket-success.module').then(m => m.TicketSuccessPageModule)
+  },
+  {
+    path: 'historic',
+    loadChildren: () => import('./pages/historic/historic.module').then(m => m.HistoricPageModule)
   },
   {
     path: '**',
     redirectTo: 'home',
     pathMatch: 'full'
-  },
-
+  }
 ];
 
 @NgModule({
