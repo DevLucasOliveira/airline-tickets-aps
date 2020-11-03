d98ba1ecf45769e43efe359cf05bca0d7b468f2b
luccasdev <lucasrti@hotmail.com>
2020-11-03 20:16:23 -0300

    Cache service with tests

--- src/app/services/ticket.service.ts
+++ src/app/services/ticket.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { HttpService } from 'src/shared/services/http.service';
 
 @Injectable({
     providedIn: 'root'
@@ -8,24 +6,8 @@ import { HttpService } from 'src/shared/services/http.service';
 export class TicketService {
 
     constructor(
-        private http: HttpService,
     ) { }
 
-    addTicket(model): Observable<any> {
-        return this.http.post(`tickets`, model);
-    }
-
-    updateTicket(ticketId, model): Observable<any> {
-        return this.http.put(`tickets/${ticketId}`, model);
-    }
-
-    getTicketById(ticketId): Observable<any> {
-        return this.http.get(`tickets/${ticketId}`);
-    }
-
-    getTickets(): Observable<any[]> {
-        return this.http.get(`tickets`);
-    }
 
 
 }
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
-import { HttpService } from './services/http.service';
 
 @NgModule({
     imports: [
@@ -10,6 +9,5 @@ import { HttpService } from './services/http.service';
         IonicModule,
         RouterModule
     ],
-    providers: [HttpService]
 })
 export class SharedModule { }
