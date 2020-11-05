df11862212922b4e85fa104c46d4890dc276df32
NickoRedko30 <nickobrasil30@gmail.com>
2020-11-04 22:56:58 -0300

    Exibir histórico somente do usuário logado

--- src/app/pages/historic/historic.page.ts
+++ src/app/pages/historic/historic.page.ts
@@ -1,6 +1,7 @@
 import { CacheService } from './../../../shared/services/cache.service';
 import { Historic } from './../../../shared/historic';
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/shared/user';
 
 @Component({
   selector: 'app-historic',
@@ -10,11 +11,14 @@ import { Component, OnInit } from '@angular/core';
 export class HistoricPage implements OnInit {
 
   historics: Historic[];
+  userEmail: string;
 
-  constructor(private cacheService: CacheService<Historic>) { }
+  constructor(private historicService: CacheService<Historic>, private userService: CacheService<User>) {
+    this.userEmail = userService.get('user').email;
+    this.historics = historicService.getAll('historic');
+  }
 
   ngOnInit() {
-    this.historics = this.cacheService.getAll('historic');
   }
 
 }
